fixed errors in velocity calculation, added command line configuration, removed old numpy velocity update

src/main.py
@@ -2,8 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from numpy.random import randn
 import time
+import argparse
 
 import taichi as ti
+import os
 
 ti.init(ti.gpu)
 
@@ -27,11 +29,11 @@ def update_velocity_ti(xs: ti.types.ndarray(), ys: ti.types.ndarray(), signs: ti
                 for yshift in shifts:
                     if k == j and xshift == 0 and yshift == 0:
                         continue
-                    x_jk = xs[k] - xs[j] + xshift
-                    y_jk = ys[k] - ys[j] + yshift
-                    r_jk = ti.sqrt(x_jk**2 + y_jk**2)
-                    vx[j] += -kappa/4/3.14159/r_jk**2*y_jk*signs[k]
-                    vy[j] += kappa/4/3.14159/r_jk**2*x_jk*signs[k]
+                    x_jk = xs[j] - xs[k] + xshift
+                    y_jk = ys[j] - ys[k] + yshift
+                    r2_jk = x_jk**2 + y_jk**2
+                    vx[j] += -kappa/4/3.14159/r2_jk*y_jk*signs[k]
+                    vy[j] += kappa/4/3.14159/r2_jk*x_jk*signs[k]
 
 @ti.kernel
 def annihilate_ti(xs: ti.types.ndarray(), ys: ti.types.ndarray(), 
@@ -77,38 +79,14 @@ class VortexPoints:
     def update_velocity(self):
         shifts = np.array([-self.D, 0, self.D])
         update_velocity_ti(self.xs, self.ys, self.signs, self.vx, self.vy, shifts)
-
-    def update_velocity_vector(self):
-        for j in range(self.N):
-            if self.signs[j] == 0:
-                continue
-            self.vx[j] = 0
-            self.vy[j] = 0
-            # vxk = 0
-            # vyk = 0
-            for xshift in [-self.D, 0, self.D]:
-                for yshift in [-self.D, 0, self.D]:
-                    x_jk = self.xs[j] - self.xs + xshift
-                    y_jk = self.ys[j] - self.ys + yshift
-                    r_jk = np.sqrt(x_jk**2 + y_jk**2)
-                    vxk = -kappa/4/np.pi/r_jk**2*y_jk*self.signs
-                    vyk = kappa/4/np.pi/r_jk**2*x_jk*self.signs
-                    if xshift == 0 and yshift == 0:
-                        vxk[j] = 0
-                        vyk[j] = 0
-                    self.vx[j] += np.sum(vxk)
-                    self.vy[j] += np.sum(vyk)
-
-                    if any(np.isnan(self.vx)):
-                        raise ValueError("nan")
     
-    def dissipation(self, alpha=0.1):
+    def dissipation(self, alpha=0.1, alphap=0):
         for j in range(self.N):
             vx0 = self.vx[j]
             vy0 = self.vy[j]
 
-            self.vx[j] += alpha*vy0*self.signs[j]
-            self.vy[j] -= alpha*vx0*self.signs[j]
+            self.vx[j] = vx0 + alpha*vy0*self.signs[j] - alphap*vx0
+            self.vy[j] = vy0 - alpha*vx0*self.signs[j] - alphap*vy0
     
     def annihilate(self, a0=a0):
         shifts = np.array([-self.D, 0, self.D])
@@ -161,14 +139,29 @@ class VortexPoints:
             
    
 if __name__ == '__main__':
-    D = 1e-2
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--D', type=float, default=1e-2)
+    parser.add_argument('--alpha', type=float, default=0.03)
+    parser.add_argument('--alphap', type=float, default=1.76e-2)
+    parser.add_argument('--output', type=str, default='output')
+    parser.add_argument('--save', action='store_true')
+
+    args = parser.parse_args()
+    D = args.D
+    alpha = args.alpha
+    alphap = args.alphap
+    output = args.output
+    save = args.save
+
+    os.makedirs(output, exist_ok=True)
+
     vp = VortexPoints(N, D)
     fig, ax = plt.subplots()
     ax.set_xlim(0, D)
     ax.set_ylim(0, D)
     ax.set_aspect('equal')
-    pos, = ax.plot(vp.xs[vp.signs > 0], vp.ys[vp.signs > 0], 'o', color='r')
-    neg, = ax.plot(vp.xs[vp.signs < 0], vp.ys[vp.signs < 0], 'o', color='b')
+    pos, = ax.plot(vp.xs[vp.signs > 0], vp.ys[vp.signs > 0], 'o', color='r', ms=2)
+    neg, = ax.plot(vp.xs[vp.signs < 0], vp.ys[vp.signs < 0], 'o', color='b', ms=2)
     t = 0
     dt = 0.0001
     last_inject = t
@@ -181,7 +174,7 @@ if __name__ == '__main__':
             print("injecting")
         vp.update_velocity()
         t1 = time.time()
-        vp.dissipation(0.01)
+        vp.dissipation(alpha, alphap)
         vp.step(dt)
         vp.annihilate()
         vp.coerce()
@@ -194,7 +187,8 @@ if __name__ == '__main__':
         plt.pause(0.01)
         N = abs(vp.signs).sum()
         print(frame, t2 - t1, t, N, vp.N)
-        fig.savefig(f'output/frame{frame:08d}.png')
+        if save:
+            fig.savefig(f'{output}/frame{frame:08d}.png')
         if N == 0:
             break
         frame += 1